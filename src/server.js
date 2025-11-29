ae7a412aaa0958e07bcd8e567de248f551c36d0e
Shihab177 <shihab7777h@gmail.com>
2025-11-29 20:01:10 +0600

    [Add]: API endpoints to fetch all users and a single user by email

--- src/server.js
+++ src/server.js
@@ -13,7 +13,37 @@ mongoose
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      success: true,
+      message: "Users fetched successfully",
+      data: users,
+    });
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: err.message,
+    });
+  }
+});
+app.get("/user", async (req, res) => {
+  try {
+    const { email } = req.query;
+    if (!email) return res.status(400).json({ success: false, message: "Email required" });
 
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ success: false, message: "User not found" });
+
+    res.status(200).send({ success: true, data: user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
 
 app.post("/user", async (req, res) => {
   try {
